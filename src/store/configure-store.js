1ccf113d03cf3ad2be44aa0744bc809ead63511e
Dukhnich_M <maryna.dukhnich@gmail.com>
2020-10-06 12:05:19 +0300

    fixed(notifications): run saga

--- src/store/configure-store.js
+++ src/store/configure-store.js
@@ -1,11 +1,17 @@
 import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+import {sagas} from "../saga/sagas";
 import rootReducer from "./root-reducer";
 import Thunk from "redux-thunk";
 import API from "./../API";
 
+const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(
     rootReducer,
-    applyMiddleware(Thunk.withExtraArgument(API))
+    applyMiddleware(Thunk.withExtraArgument(API), sagaMiddleware)
 );
+
+sagaMiddleware.run(sagas);
 
 export default store;
